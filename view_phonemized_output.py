53b8df8827d574cf9d2a47ed617c28be9b8019bf
AntoniaJJJJ <167152220+AntoniaJJJJ@users.noreply.github.com>
2024-09-19 15:28:39 +1000

    view phonemized output

---
+++ view_phonemized_output.py
@@ -0,0 +1,11 @@
+from datasets import load_from_disk
+
+# Path to the processed dataset
+output_dataset_path = "/srv/scratch/z5369417/outputs/phonemization2_remove/cu/train"
+
+# Load the dataset
+dataset = load_from_disk(output_dataset_path)
+
+# Iterate over all examples and print them
+for example in dataset:
+    print(example)
