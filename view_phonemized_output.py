5aa7ad655873b32705874658ce037bd0766644cf
AntoniaJJJJ <167152220+AntoniaJJJJ@users.noreply.github.com>
2024-09-24 16:49:45 +1000

    update view_output

--- view_phonemized_output.py
+++ view_phonemized_output.py
@@ -1,7 +1,7 @@
 from datasets import load_from_disk
 
 # Path to the processed dataset
-output_dataset_path = "/srv/scratch/z5369417/outputs/phonemization2_remove/cu/train"
+output_dataset_path = "/srv/scratch/z5369417/outputs/phonemization2_remove/cu_long3hrs/train"
 
 # Load the dataset
 dataset = load_from_disk(output_dataset_path)
