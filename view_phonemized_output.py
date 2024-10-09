d84204df55115b7040498ec5cbc307c5c1a00aad
AntoniaJJJJ <167152220+AntoniaJJJJ@users.noreply.github.com>
2024-10-09 15:46:18 +1100

    view data

--- view_phonemized_output.py
+++ view_phonemized_output.py
@@ -1,7 +1,7 @@
 from datasets import load_from_disk
 
 # Path to the processed dataset
-output_dataset_path = "/srv/scratch/z5369417/outputs/phonemization2_remove/cu_long3hrs/train"
+output_dataset_path = "/srv/scratch/z5369417/created_dataset_0808/myst_dataset/train"
 
 # Load the dataset
 dataset = load_from_disk(output_dataset_path)
