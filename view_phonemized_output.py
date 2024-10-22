8442cab5927a6355935d9feb408f9f4a1d68a2cc
AntoniaJJJJ <167152220+AntoniaJJJJ@users.noreply.github.com>
2024-10-22 16:35:09 +1100

    view data

--- view_phonemized_output.py
+++ view_phonemized_output.py
@@ -1,7 +1,7 @@
 from datasets import load_from_disk
 
 # Path to the processed dataset
-output_dataset_path = "/srv/scratch/z5369417/created_dataset_0808/AKT_dataset/train"
+output_dataset_path = "/srv/scratch/z5369417/created_dataset_0808/AKT_dataset/test"
 
 # Load the dataset
 dataset = load_from_disk(output_dataset_path)
