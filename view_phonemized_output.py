fb3adee1da940bdf79ba0cfcf3ac0827e7a994f3
AntoniaJJJJ <167152220+AntoniaJJJJ@users.noreply.github.com>
2024-10-27 18:33:22 +1100

    fix bug

--- view_phonemized_output.py
+++ view_phonemized_output.py
@@ -7,7 +7,7 @@ output_dataset_path = "/srv/scratch/z5369417/created_dataset_3009/AKT_exclude_no
 dataset = load_from_disk(output_dataset_path)
 
 # Iterate over all examples and print them
-for example in datasetS:
+for example in dataset:
     print(example)
 
 # Iterate over the first 2000 examples and print them
