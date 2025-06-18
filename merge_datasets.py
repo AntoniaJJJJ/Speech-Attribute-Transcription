6061b1ddb46b0f4b76866db486960980a8285a0a
AntoniaJJJJ <167152220+AntoniaJJJJ@users.noreply.github.com>
2025-06-18 21:27:46 +1000

    fix

--- merge_datasets.py
+++ merge_datasets.py
@@ -1,4 +1,5 @@
-from datasets import load_from_disk, concatenate_datasets, DatasetDict
+from datasets import load_from_disk, concatenate_datasets, DatasetDict, Value
+import datasets
 
 # Load original datasets
 cu = load_from_disk('/srv/scratch/z5369417/outputs/phonemization2_remove/cu_remove_adult_noise')
@@ -7,6 +8,10 @@ akt = load_from_disk('/srv/scratch/z5369417/outputs/phonemization_AKT')
 # Rename phoneme column to be consistent across both
 cu = cu.rename_column('phoneme_cmu', 'phoneme_combined')
 akt = akt.rename_column('phoneme_akt', 'phoneme_combined')
+
+# Harmonize data types
+cu = cu.cast_column('speaker_id', Value('string'))
+akt = akt.cast_column('speaker_id', Value('string'))
 
 # Merge by split
 train = concatenate_datasets([cu['train'], akt['train']])
